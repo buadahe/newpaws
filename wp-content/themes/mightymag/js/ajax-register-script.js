e020db018e39a074cc050dc0a3ace022b3a5b384
Joko Susilo <jksusilo28@gmail.com>
2017-01-26 19:08:36 +0700

    Add registration function

--- wp-content/themes/mightymag/js/ajax-register-script.js
+++ wp-content/themes/mightymag/js/ajax-register-script.js
@@ -2,7 +2,12 @@
  * Created by Adi Umbas Primadarma on 1/7/2017.
  */
 
-$(document).ready(function($) {
+jQuery(document).ready(function($) {
+    // Function to set checked radio button
+    jQuery('#signup_form #signup_category').change(function(event) {
+        
+    });
+
 
     // Show the login dialog box on click
     //$('a#show_login').on('click', function(e){
@@ -16,26 +21,63 @@ $(document).ready(function($) {
     //});
 
     // Perform AJAX login on form submit
-    $('#registerss').on('submit', function(e){
-        $('#registerss p.status').show().text(ajax_login_object.loadingmessage);
+    // jQuery('#loginform').on('submit', function(e){
+    //     jQuery('#loginform p.status').show().text(ajax_login_object.loadingmessage);
+    //     $.ajax({
+    //         type: 'POST',
+    //         url: 'http://localhost/newpaws/wp-login.php',
+    //         cache: false,
+    //         data: {
+    //             'log'    : jQuery('#loginform #user_login').val(),
+    //             'pwd'    : jQuery('#loginform #user_pass').val()
+    //         },
+    //         success: function(data){
+    //             $('#loginform p.status').text(data.message);
+    //             if (data.loggedin == true){
+    //                 document.location.href = ajax_login_object.redirecturl;
+    //             }
+    //         },
+    //         error: function (xhr) {
+    //             alert('error');
+    //             // $('p:first', form[0]).before(
+    //             //     $(document.createElement('div'))
+    //             //         .html('<strong>ERROR</strong>: ' + xhr.statusText)
+    //             //         .attr('id', 'login_error')
+    //             // );
+    //             // activity.hide(); fields.show();
+    //         }
+    //     });
+    //     e.preventDefault();
+    // });
+
+    // Perform AJAX register on form submit
+    jQuery('#signup_form').on('submit', function(e){
+        jQuery('#signup_form p.status').show().text(ajax_login_object.loadingmessage);
         $.ajax({
             type: 'POST',
             dataType: 'json',
             url: ajax_login_object.ajaxurl,
             data: {
-                'action': 'ajaxregister', //calls wp_ajax_nopriv_ajaxlogin
-                'username': $('#registerss #name').val(),
-                'password': $('#registerss #password').val(),
-                'category': $('#registerss #category').val(),
-                'security': $('#registerss #security').val() },
+                'action'            : 'ajaxregister', //calls wp_ajax_nopriv_ajaxlogin
+                'signup_email'      : jQuery('#signup_form #signup_email').val(),
+                'signup_username'   : jQuery('#signup_form #signup_username').val(),
+                'signup_password'   : jQuery('#signup_form #signup_password').val(),
+                'signup_category'   : jQuery('#signup_form input[name=signup_category]:checked').val(),
+                'security'          : jQuery('#signup_form #security').val() 
+            },
             success: function(data){
-                $('#registerss p.status').text(data.message);
                 if (data.loggedin == true){
-                    document.location.href = ajax_login_object.redirecturl;
+                    // console.log('sukses');
+                    window.location.href = ajax_login_object.redirecturl;
+                }else{
+                    // console.log('gagal');
+                    alert(data.message);
                 }
             }
         });
         e.preventDefault();
     });
+
+    
 
 });
